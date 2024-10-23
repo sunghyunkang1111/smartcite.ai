712cdab6d878071a8aa78b8c66fcfb0f3f336577
le-roi-dev <chry.dev@outlook.com>
2024-10-23 17:16:11 -0400

    Update: .env file

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,19 +1,21 @@
 import KeycloakProvider from "next-auth/providers/keycloak";
+const clientId = process.env.CLIENT_ID as string;
+const clientSecret = process.env.CLIENT_SECRET as string;
+const issuer = process.env.ISSUER;
 
 const authOptions = {
   // Configure one or more authentication providers
   providers: [
     // !!! Should be stored in .env file.
     KeycloakProvider({
-      clientId: `smartcite-client`,
-      clientSecret: `3QMUQY74n9ta7fSNRukURPSjZuegp1En`,
-      issuer: `http://localhost:8080/realms/smartcite-realm`,
+      clientId: clientId,
+      clientSecret: clientSecret,
+      issuer: issuer,
       profile(profile) {
         return {
           id: profile.sub,
           name: profile.name ?? profile.preferred_username,
           email: profile.email,
-          image: `https://faces-img.xcdn.link/thumb-lorem-face-6312_thumb.jpg`,
         };
       },
     }),
