711666da50fd513404d57101745c56213fcbedb0
le-roi-dev <chry.dev@outlook.com>
2024-10-23 17:13:45 -0400

    Remove: Unnecessary Pages

--- src/app/_refine_context.tsx
+++ src/app/_refine_context.tsx
@@ -124,35 +124,8 @@ const App = (props: React.PropsWithChildren<AppProps>) => {
               },
             },
             {
-              name: "citations",
-              list: "/citations",
-              meta: {
-                canDelete: true,
-              },
-            },
-            {
               name: "api-keys",
               list: "/api-keys",
-              meta: {
-                canDelete: true,
-              },
-            },
-            {
-              name: "blog_posts",
-              list: "/blog-posts",
-              create: "/blog-posts/create",
-              edit: "/blog-posts/edit/:id",
-              show: "/blog-posts/show/:id",
-              meta: {
-                canDelete: true,
-              },
-            },
-            {
-              name: "categories",
-              list: "/categories",
-              create: "/categories/create",
-              edit: "/categories/edit/:id",
-              show: "/categories/show/:id",
               meta: {
                 canDelete: true,
               },
